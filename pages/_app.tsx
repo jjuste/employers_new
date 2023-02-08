4c97a123e9c3c40cddc8456b249cc29fbafebb00
Jean Juste <jean.juste@its.ny.gov>
2023-02-08 10:33:40 -0500

    debugging

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,11 @@
 import '@/styles/globals.css'
+import './css/ccwb.css'
+import './css/nys-global-nav-fonts.css'
+import './css/nys-global-nav.css'
+import './css/nys-brand/health-and-human-services.css'
+import './css/language.css'
+// import './fonts/fontawesome/css/all.min.css'
+import './fonts/icomoon/style.css'
 
 import type { AppProps } from 'next/app'
 
